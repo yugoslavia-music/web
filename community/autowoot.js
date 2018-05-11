376dc6677ceb9c4b41e0183ea7cfe1a835ff142a
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-05-12 00:00:18 +0200

    Create autowoot.js

---
+++ community/autowoot.js
@@ -0,0 +1,5 @@
+//Throw this in your console tab, F12 at browser then console tab. Refresh your page to unload the script.
+API.chatLog("Autowoot Aktiviran");
+$('.btn-like').click(); 
+API.on(API.DJ_ADVANCE, callback); 
+function callback(obj) { $('.btn-like').click(); }
