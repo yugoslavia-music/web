1c7988446dbe958f3954749835f5abb15cda2db1
Ajdin Alic <ajdin1997@users.noreply.github.com>
2018-05-12 00:03:18 +0200

    Update autowoot.js

--- community/autowoot.js
+++ community/autowoot.js
@@ -1,5 +1,5 @@
 //Throw this in your console tab, F12 at browser then console tab. Refresh your page to unload the script.
-API.chatLog("Autowoot Aktiviran");
+API.chatLog("Autowoot Aktiviran - Yugoslavia Balkan Music v1");
 $('.btn-like').click(); 
 API.on(API.DJ_ADVANCE, callback); 
 function callback(obj) { $('.btn-like').click(); }
